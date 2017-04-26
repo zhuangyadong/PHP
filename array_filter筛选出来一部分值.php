ec3f977097e4cd1469144a9b4ac8d81d2a2b7434
zhuangyadong <876573617@qq.com>
2017-04-26 17:09:58 +0800

    Update array_filter筛选出来一部分值.php

--- array_filter筛选出来一部分值.php
+++ array_filter筛选出来一部分值.php
@@ -1,6 +1,6 @@
 <?php
 
-$arr=array("user1"=>40,"user2"=>50,"user3"=>60,"user4"=>70,"user5"=>80);
+$arr=array("user1"=>40,"user2"=>50,"user3"=>60,"user4"=>70,"user5"=>80,"user6"=>90);
 function jige($val){
     if($val>=60){
         return true;
@@ -8,6 +8,9 @@ function jige($val){
 }
 
 $arr2=array_filter($arr,"jige");
+
+
+
 
 echo "<pre>";
 print_r($arr2);
