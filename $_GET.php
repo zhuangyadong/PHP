f57dde275254167290200847f4f3297000b45e4e
zhuangyadong <876573617@qq.com>
2017-05-01 23:40:23 +0800

    Update $_GET.php

--- $_GET.php
+++ $_GET.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>get传值</title>
+    <title>get传输数据</title>
 </head>
 <body>
     <h1>查看用户详情信息</h1>
