8425488fbc01d42a5aa6ea7b966949022edf474f
zhuangyadong <876573617@qq.com>
2017-09-29 22:21:14 +0800

    Update asort保留key排序.php

--- asort保留key排序.php
+++ asort保留key排序.php
@@ -1,10 +1,7 @@
 <?php
     header("content-type:text/html;charset=utf-8");
-
     $arr = array("a"=>10,"b"=>20,"c"=>30,"d"=>40);
-
     asort($arr);
-
     echo "<pre>";
     print_r($arr);
     echo "</pre>";
